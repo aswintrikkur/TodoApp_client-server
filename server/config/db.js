84cab79aa91447080788c4e95723b6ab2ff8ee4e
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-10-04 09:22:00 +0530

    deployment trial 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
 const  mongoose  = require("mongoose");
 require('dotenv').config()
+const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
-        const {connection} = await mongoose.connect(process.env.MONGO_URI);
+        const {connection} = await mongoose.connect(MONGO_URI);
         // console.log('db connected');
          console.log('dataBase connected :', connection.host);
     } catch (error) {
         console.log(error);
-        throw(error);
     }
 
 }
